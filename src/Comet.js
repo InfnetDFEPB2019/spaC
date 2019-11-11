34d950f3c97c02763dc4760922d0b125c80e5828
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2019-11-11 10:03:12 -0300

    Trying to solve a problem with a Data object, refactored the code.

--- src/Comet.js
+++ src/Comet.js
@@ -1,7 +1,117 @@
-// // id, name, absolute magnitude, estimated diameter (km), potentially hazardous asteroid,
-// // close approach date (full), relative velocity, miss distance (km), sentry object
-//
-// /*export default*/ class Comet {
+export default class Comet {
+    constructor(id, name, absoluteMagnitude, estimatedDiameterAvg, potentiallyHazardous,
+                closeApproach, relativeVelocity, missDistance, sentryObject) {
+        this.Id = id;
+        this.Name = name;
+        this.AbsoluteMagnitude = absoluteMagnitude;
+        this.EstimatedDiameterAvg = estimatedDiameterAvg;
+        this.PotentiallyHazardous = potentiallyHazardous;
+        this.CloseApproach = closeApproach;
+        this.RelativeVelocity = relativeVelocity;
+        this.MissDistance = missDistance;
+        this.SentryObject = sentryObject;
+    }
+
+    get id() {
+        return this.Id;
+    }
+
+    set id(value) {
+        this.Id = value;
+    }
+
+    get name() {
+        return this.Name;
+    }
+
+    set name(value) {
+        this.Name = value;
+    }
+
+    get absoluteMagnitude() {
+        return this.AbsoluteMagnitude;
+    }
+
+    set absoluteMagnitude(value) {
+        this.AbsoluteMagnitude = value;
+    }
+
+    get estimatedDiameterAvg() {
+        return this.EstimatedDiameterAvg;
+    }
+
+    set estimatedDiameterAvg(value) {
+        this.EstimatedDiameterAvg = value;
+    }
+
+    get potentiallyHazardous() {
+        return this.PotentiallyHazardous;
+    }
+
+    set potentiallyHazardous(value) {
+        this.PotentiallyHazardous = value;
+    }
+
+    get closeApproach() {
+        return this.CloseApproach;
+    }
+
+    set closeApproach(value) {
+        this.CloseApproach = value;
+    }
+
+    get relativeVelocity() {
+        return this.RelativeVelocity;
+    }
+
+    set relativeVelocity(value) {
+        this.RelativeVelocity = value;
+    }
+
+    get missDistance() {
+        return this.MissDistance;
+    }
+
+    set missDistance(value) {
+        this.MissDistance = value;
+    }
+
+    get sentryObject() {
+        return this.SentryObject;
+    }
+
+    set sentryObject(value) {
+        this.SentryObject = value;
+    }
+
+    static ceilValue(value){
+        return Math.ceil(value);
+    }
+
+    static hazardLevel(String){
+        let classification;
+
+        if (String === "true") {
+            classification = "DANGER";
+        } else {
+            classification = "SAFE";
+        }
+        return classification;
+    }
+
+    static hitChanceNext100Years(String){
+        let possibility;
+
+        if (String === "true") {
+            possibility = "MAYBE";
+        } else {
+            possibility = "SAFE";
+        }
+        return possibility;
+    }
+}
+
+// export default class Comet {
 //     constructor(id, name, absoluteMagnitude, estimatedDiameter, potentiallyHazardous,
 //                 closeApproach, relativeVelocity, missDistance, sentryObject) {
 //         this.Id = id;
